4e3abe317b21131dadfb239106bc9570cd51009a
Aguinaldo Bezerra <aguinaldoabbj@gmail.com>
2019-09-18 19:00:30 -0300

    adjust worker exposed ports

--- master-livy/start-livy.sh
+++ master-livy/start-livy.sh
@@ -14,6 +14,8 @@ source $HOME/.bashrc
 sed -i "/\# livy.spark.master =/c\livy.spark.master = spark:\/\/$(hostname -i):$(echo $SPARK_MASTER_PORT)" ${LIVY_HOME}/conf/livy.conf 
 sed -i "/\# livy.spark.deploy-mode =/c\livy.spark.deploy-mode = client" ${LIVY_HOME}/conf/livy.conf 
 
+#Adjust spark confs
+
 #Start livy
 /bin/bash ${LIVY_HOME}/bin/livy-server start & tail -f /dev/null #stay live as live-server exits
  
