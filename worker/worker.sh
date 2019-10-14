51710e4c2494afd94056846aa533eef163a8b662
Aguinaldo Bezerra <aguinaldoabbj@users.noreply.github.com>
2019-10-14 17:04:13 -0300

    Create worker.sh

---
+++ worker/worker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. "/spark/sbin/spark-config.sh"
+
+. "/spark/bin/load-spark-env.sh"
+
+mkdir -p $SPARK_WORKER_LOG
+
+export SPARK_HOME=/spark
+
+ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
+
+echo "$MASTER_IP $MASTER_HOST" >> /etc/hosts
+
+/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker \
+    --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
