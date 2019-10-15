c7fb66cb7d5260cb2234a6f6768b3a878523a8d3
Aguinaldo Bezerra <aguinaldoabbj@users.noreply.github.com>
2019-10-15 11:01:52 -0300

    Update worker.sh

--- worker/worker.sh
+++ worker/worker.sh
@@ -10,7 +10,7 @@ export SPARK_HOME=/spark
 
 ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
 
-echo "$MASTER_IP $MASTER_HOST" >> /etc/hosts
+echo "$MASTER_IP    $MASTER_HOST" >> /etc/hosts
 
 /spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker \
     --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
